fc92893d6710a88df4dca4510e62ad99567ffd2d
Jarod Peachey <jwpeachey107@aol.com>
2021-10-09 15:41:14 -0400

    Test updates third time

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,24 +2,24 @@ import * as React from "react";
 import { StoreProvider } from "./src/context/store-context";
 import "./src/styles/global.css";
 
-function addScript(url, id) {
-  const script = document.createElement("script");
-  script.src = url;
-  script.async = true;
-  script.defer = true;
-  script.id = id;
-  document.body.appendChild(script);
-}
+// function addScript(url, id) {
+//   const script = document.createElement("script");
+//   script.src = url;
+//   script.async = true;
+//   script.defer = true;
+//   script.id = id;
+//   document.body.appendChild(script);
+// }
 
 export const wrapRootElement = ({ element }) => <StoreProvider>{element}</StoreProvider>;
 
-export const onRouteUpdate = ({ location, prevLocation }) => {
-  console.log("new pathname", location.pathname);
-  console.log("old pathname", prevLocation ? prevLocation.pathname : null);
+// export const onRouteUpdate = ({ location, prevLocation }) => {
+//   console.log("new pathname", location.pathname);
+//   console.log("old pathname", prevLocation ? prevLocation.pathname : null);
 
-  if (location.pathname.includes("product")) {
-    addScript("/js/options.js", "options-script");
-  } else {
-    document.getElementById("options-script") && document.getElementById("options-script").remove();
-  }
-};
+//   if (location.pathname.includes("product")) {
+//     addScript("/js/options.js", "options-script");
+//   } else {
+//     document.getElementById("options-script") && document.getElementById("options-script").remove();
+//   }
+// };
