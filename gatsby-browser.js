1937f611c902f78c8243c5cc0542c86be5159fe6
Jarod Peachey <jwpeachey107@aol.com>
2021-10-09 15:56:33 -0400

    Testing updates

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,16 +2,20 @@ import * as React from "react";
 import { StoreProvider } from "./src/context/store-context";
 import "./src/styles/global.css";
 
-// function addScript(url, id) {
-//   const script = document.createElement("script");
-//   script.src = url;
-//   script.async = true;
-//   script.defer = true;
-//   script.id = id;
-//   document.body.appendChild(script);
-// }
+function addScript(url, id) {
+  const script = document.createElement("script");
+  script.src = url;
+  script.async = true;
+  script.defer = true;
+  script.id = id;
+  document.body.appendChild(script);
+}
 
 export const wrapRootElement = ({ element }) => <StoreProvider>{element}</StoreProvider>;
+
+export const onRouteUpdate = () => {
+  addScript('/js/options.js')
+}
 
 // export const onRouteUpdate = ({ location, prevLocation }) => {
 //   console.log("new pathname", location.pathname);
