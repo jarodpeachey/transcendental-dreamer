1a603fc9ceb77fe00e93f17559eb4c20578620a2
Jarod Peachey <jwpeachey107@aol.com>
2021-10-09 15:28:36 -0400

    Updates

--- static/js/options.js
+++ static/js/options.js
@@ -1,17 +1,21 @@
 (function () {
   // ELEMENTS
   const wrappers = document.querySelectorAll('.options')
+      console.log(wrappers);
+    alert(wrappers)
 
   Array.from(wrappers).forEach(item => {
     const options = item.querySelectorAll('.option')
-
-      Array.from(options).forEach(item => {
-    item.addEventListener('click', (e) => {
-      Array.from(options).forEach(item => {
-        item.classList.remove('active')
+    console.log(options);
+    alert(options)
+  
+    Array.from(options).forEach(item => {
+      item.addEventListener('click', (e) => {
+        Array.from(options).forEach(item => {
+          item.classList.remove('active')
+        })
+        e.target.classList.add('active')
       })
-      e.target.classList.add('active')
     })
-  })
   })
 })();
